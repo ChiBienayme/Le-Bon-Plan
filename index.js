f08f682f519c781b8ceb9681e806a9c175588cda
Ha Chi HOANG <chibienayme@gmail.com>
2022-04-05 14:09:10 +0200

    Add routers: homepage + signup  + login + profile

--- index.js
+++ index.js
@@ -15,14 +15,78 @@ app.use(express.static(path.join(__dirname, "/public")));
 // body du form de login
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
-app.get("/", (_req, res) => {
+// ! Routes
+// TODO Homepage
+app.get("/", (req, res) => {
 	// renvoie un fichier html
 	res.render("homepage", {
-		
+		isLoggedIn: false,
 	});
 });
 
+// TODO Login
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+app.post("/login", (req, res) => {
+	// créer un utilisateur
+
+	console.log(req.body);
+
+	// créer token
+	const token = "eyJqdqsdsqfg";
+
+	res.cookie("jwt", token);
+
+	res.redirect("/profile");
+});
+
+// TODO Signup
+app.get("/signup", (req, res) => {
+	res.render("signup");
+});
+
+app.post("/signup", (req, res) => {
+	// créer un utilisateur
+
+	console.log(req.body);
+
+	// créer token
+	const token = "eyJqdqsdsqfg";
+
+	res.cookie("jwt", token);
+
+	res.redirect("/profile");
+});
+
+// TODO Profile
+app.get("/profile", async (req, res) => {
+	const token = req.cookies.jwt; // eyJfvdfhv5656fvdxfsd
+	const userId = 23;
+
+	if (!token) {
+		return res.redirect("/");
+	}
+
+	// const userData = await User.findById(userId);
+	// const userData = await Pool.query("SELECT * FROM users WHERE user_id=$1", [
+	// 	userId,
+	// ]);
+
+	res.render("profile", {
+		// name: userData.name,
+		name: "Nicolas",
+		email: "nico@gmail.com",
+	});
+});
+
+// TODO Users
+app.get("/users", (req, res) => {
+	res.json({
+		name: "Jean",
+	});
+});
 
 // Start server
 app.listen(8000, () => console.log("Listening"));
